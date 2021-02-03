421eed29f615f6a9d3fd3ff62d54b5099bdefbf6
M-Sieger <msieger1994@gmail.com>
2021-02-03 17:01:36 +0100

    add utils function

--- src/components/form/verifyform.js
+++ src/components/form/verifyform.js
@@ -1,22 +1,30 @@
+import { createElement } from "../form/createElement";
+
 function createOTPInputElement() {
-  const input = document.createElement("input");
-  input.type = "password";
-  input.placeholder = "*";
-  input.className = "input";
+  const input = createElement("input", {
+    className: "input",
+    placeholder: "*",
+    type: "password",
+  });
 
   return input;
 }
 
 function createPasswordContainerElement() {
-  const passwordContainer = document.createElement("div");
-  passwordContainer.className = "form__otp";
+  /*const passwordContainer = document.createElement("div");
+  passwordContainer.className = "form__otp";*/
 
   const otpOne = createOTPInputElement();
   const otpTwo = createOTPInputElement();
   const otpThree = createOTPInputElement();
   const otpFour = createOTPInputElement();
 
-  passwordContainer.append(otpOne, otpTwo, otpThree, otpFour);
+  const passwordContainer = createElement("div", {
+    className: "form__otp",
+    children: [otpOne, otpTwo, otpThree, otpFour],
+  });
+
+  /*passwordContainer.append(otpOne, otpTwo, otpThree, otpFour);*/
 
   return passwordContainer;
 }
